baae159980ebdd01a442e2ea00c000798293f2c1
wyntergos <wynandgouwswg@gmail.com>
2017-03-17 21:26:05 +0800

    Finished adding borg features
    Ready for pull-request. All borg feautures should be working to a
    minimum standard

--- locutus.sh
+++ locutus.sh
@@ -28,6 +28,13 @@ acm_path=""
 # Borg (FULL PATH)(leave BLANK for default)
 borg_path=""
 
+# Borg prune options
+borg_keep_hourly="24"
+borg_keep_daily="7"
+borg_keep_weekly="4"
+borg_keep_monthly="12"
+borg_keep_yearly="10"
+
 # Gmail (FULL PATH)(leave BLANK for default)
 gmv_path="/home/wynand/.virtualenv2/gmvault/bin/gmvault"
 email_address="wynandgouwswg@gmail.com"
@@ -45,7 +52,7 @@ gmv_save_location=$backup_location$gmv_save_location
 excluded_folders="--exclude "${excluded_folders//,/\ --exclude}
 excluded_folders=${excluded_folders//\"/ }
 excluded_patterns=${excluded_patterns//\ /}
-excluded_patterns="--exclude '"${excluded_patterns//,/\' --exclude \'}"'"
+excluded_patterns="--exclude "${excluded_patterns//,/\ --exclude }
 backed_up_files=${backed_up_files//,/\ }
 backed_up_files=${backed_up_files//\"/ }
 
@@ -86,64 +93,49 @@ notify-send "Backup Started"""
 # crontab -l > /home/wynand/GoogleDrive/01_Personal/05_Software/Antergos/wyntergos_crontab
 
 # Create backups of save locations
-borg init $backup_location
-borg create -p -C lz4 $borg_save_location::"{hostname}-{now:%Y%m%d-%H%M}" $backed_up_files $excluded_folders $excluded_patterns
+borg init $borg_save_location
+borg create $excluded_folders $excluded_patterns -p -C lz4 $borg_save_location::"{hostname}-{now:%Y%m%d-%H%M}" $backed_up_files
 
 # Backup Gmail using gmvault
-expect <<- DONE
-    set timeout -1
-    spawn $gmv_path sync -e -d $gmv_save_location $email_address -p
-    match_max 100000
-    expect -re {Please enter gmail password for }
-    send "$GOOGLE_PASSPHRASE"
-    send -- "\r"
-    expect eof
-DONE
+#expect <<- DONE
+    #set timeout -1
+    #spawn $gmv_path sync -e -d $gmv_save_location $email_address -p
+    #match_max 100000
+    #expect -re {Please enter gmail password for }
+    #send "$GOOGLE_PASSPHRASE"
+    #send -- "\r"
+    #expect eof
+#DONE
 
 # Save packages and configurations using aconfmgr
-expect <<- DONE
-    set timeout -1
-    spawn aconfmgr -c $acm_save_location save
-    match_max 100000
-    expect -re {\[0m\[sudo\] password for }
-    send "$SUDO_PASSPHRASE"
-    send -- "\r"
-    expect eof
-DONE
+#expect <<- DONE
+    #set timeout -1
+    #spawn aconfmgr -c $acm_save_location save
+    #match_max 100000
+    #expect -re {\[0m\[sudo\] password for }
+    #send "$SUDO_PASSPHRASE"
+    #send -- "\r"
+    #expect eof
+#DONE
 
-#Prune Backups
-# echo "Pruning........."
-# borg prune /wynZFS/Wynand/Backups/Antergos/ --prefix "{hostname}-" --keep-hourly=24 --keep-daily=7 --keep-weekly=4 --keep-monthly=12 --keep-yearly=10
+# Prune Backups
+echo "Pruning........."
+borg prune $borg_save_location --prefix "{hostname}-" --keep-hourly=$borg_keep_hourly --keep-daily=$borg_keep_daily --keep-weekly=$borg_keep_weekly --keep-monthly=$borg_keep_monthly --keep-yearly=$borg_keep_yearly
 
 # Check backups and alert if issues
-# echo "Checking........"
-# borg check /wynZFS/Wynand/Backups/Antergos/ &>> /home/wynand/wynZFS/Wynand/Backups/.tmp.txt
+echo "Checking........"
+borg check $borg_save_location &>> $borg_save_location/.tmp.txt
 
-# if grep -Fq "Completed repository check, errors found" /home/wynand/wynZFS/Wynand/Backups/.tmp.txt
-# then
-#     notify-send "Backup Error" "There was an error found in one of the Borg backups"
-#   rm -rf /home/wynand/wynZFS/Wynand/Backups/.tmp.txt
-#     mv /home/wynand/wynZFS/Wynand/Backups/.tmp.txt /home/wynand/BorgCheck.txt
-# else
-#     rm -rf /home/wynand/wynZFS/Wynand/Backups/.tmp.txt
-#     notify-send "Backups Checked" "All clear"
+if grep -Fq "Completed repository check, errors found" $borg_save_location/.tmp.txt
+then
+    notify-send "Backup Error" "There was an error found in one of the Borg backups"
+    mv $borg_save_location/.tmp.txt ~/BorgErrors.txt
+else
+    rm -rf $borg_save_location/.tmp.txt
+    notify-send "Backups Checked" "All clear"
     # Only copy files to HDD and mega if no errors
-   
-#   echo "Finding changed files..."
-#   # Need to see if any files changed, and delete them from mega so that the new files can be uploaded
-#   diff -qrN /wynZFS/Wynand/Backups /run/media/wynand/Wyntergos_Backups/Backups | cut -d \  -f 4 >/home/wynand/wynZFS/Wynand/Backups/.tmp.txt
-#   cut -d \  -f 2 /home/wynand/wynZFS/Wynand/Backups/.changes >/home/wynand/wynZFS/Wynand/Backups/.tmp.txt
-#   rm -rf /home/wynand/wynZFS/Wynand/Backups/.changes
-#   sed -i 's/\/home\/wynand\/wynZFS\/Wynand\//\/run\/media\/wynand\/Wyntergos_Backups\//g' /home/wynand/wynZFS/Wynand/Backups/.tmp.txt
-#   cat /home/wynand/wynZFS/Wynand/Backups/.tmp.txt | xargs -i rm -rf {}
-#   sed -i 's/\/run\/media\/wynand\/Wyntergos_Backups\//\/Root\//g' /home/wynand/wynZFS/Wynand/Backups/.tmp.txt
-#   cat /home/wynand/wynZFS/Wynand/Backups/.tmp.txt | xargs -i megarm -u ${mega_user} -p ${mega_password} {}
-#   rm -rf /home/wynand/wynZFS/Wynand/Backups/.tmp.txt
 
-# finding changes seems to cause integrity issues, will try just using the official mega app and closing it when done
-
-
-    echo "Copying........."
+echo "Copying........."
     # Copy to External Drive
 #     cp -Lruv /home/wynand/wynZFS/Wynand/Backups /run/media/wynand/Wyntergos_Backups
 
@@ -151,7 +143,7 @@ DONE
 #     echo "Uploading......."
 #   nocorrect megacopy -u ${mega_user} -p ${mega_password} -r /Root/Backups -l  /wynZFS/Wynand/Backups
 #     megasync
-# fi
+fi
 
 # kill $(pgrep megasync)
 
